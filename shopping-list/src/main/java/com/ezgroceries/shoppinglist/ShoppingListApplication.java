c87891fe04ead1600e2079a771ffe88d7a58ed6e
Michiel Abé <michiel.abe@kbc.be>
2020-06-07 22:43:54 +0200

    Added swagger

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
@@ -2,6 +2,11 @@ package com.ezgroceries.shoppinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class ShoppingListApplication {
@@ -10,4 +15,12 @@ public class ShoppingListApplication {
         SpringApplication.run(ShoppingListApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/shoppinglists/controller/ShoppingListController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/shoppinglists/controller/ShoppingListController.java
@@ -15,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
 @RequestMapping(produces = "application/json")
+@EnableSwagger2
 public class ShoppingListController {
 
     @Autowired
